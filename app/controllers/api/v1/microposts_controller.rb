7febba48df2a92faffe179a8ec6765d89a6fdf25
EC2 Default User <ec2-user@ip-10-0-0-59.ap-northeast-1.compute.internal>
2023-10-19 15:18:03 +0000

    user and post

--- app/controllers/api/v1/microposts_controller.rb
+++ app/controllers/api/v1/microposts_controller.rb
@@ -1,18 +1,21 @@
 class Api::V1::MicropostsController < ApplicationController
-        def create
-            @user=user.find_by(id:params[:id])
-           if @post = @user.microposts.build(microposts_params)
-              @post.save
-                render json: {status: 201, data: microposts}
-           else
-            render json: {status: 400,error: "micorposts can't found"}
-           end
-        end
-        private
-        def microposts_params
-          params.require(:microposts).permit(:title,:body,:user_id)
-        end
-    
+    def create
+      user = User.find(params[:user][:id]) # "user"オブジェクトから"id"を取得
+      @micropost = user.microposts.build(microposts_params)
+  
+      if @micropost.save
+        render json: { status: 201, data: @micropost }
+      else
+        render json: { status: 400, error: "micorposts can't be found" }
+      end
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { status: 404, error: "User not found" }
     end
-    
-end
+  
+    private
+  
+    def microposts_params
+      params.require(:micropost).permit(:title, :body, :user_id)
+    end
+  end
+  
