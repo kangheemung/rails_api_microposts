df47019b651b113ba6103a7f28f7dded09ecdf1a
EC2 Default User <ec2-user@ip-10-0-0-59.ap-northeast-1.compute.internal>
2023-10-22 06:21:57 +0000

    micropostAPIcreate

--- app/controllers/api/v1/microposts_controller.rb
+++ app/controllers/api/v1/microposts_controller.rb
@@ -1,16 +1,68 @@
 class Api::V1::MicropostsController < ApplicationController
-    def create
-      user = User.find(params[:user][:id]) # "user"オブジェクトから"id"を取得
-      @micropost = user.microposts.build(microposts_params)
-  
-      if @micropost.save
-        render json: { status: 201, data: @micropost }
-      else
-        render json: { status: 400, error: "micorposts can't be found" }
-      end
-    rescue ActiveRecord::RecordNotFound => e
-      render json: { status: 404, error: "User not found" }
+  def index
+    #posts =Post.limit(100)#≤ =データ多い時
+        #if posts.count > 0
+        #render json: {status: 201, data: posts }
+        #else
+        #render json:{status:400, error: "posts can't found"}
+        #end
+    #puts "user parameter check"
+    #puts params
+    page = params[:page].to_i || 1
+    per_page = 100
+
+    offset = (page - 1) * per_page
+    @microposts = Micropost.order(created_at: :desc).offset(offset).limit(per_page)
+
+    @micropost_data = []
+    microposts.each do |micropost|
+      @micropost_data << { title: micropost.title, body: micropost.body }
     end
+
+    render json: @micropost_data
+  
+    end
+      def create
+        user = User.find(params[:user][:id]) # "user"オブジェクトから"id"を取得
+          p"====================="
+          p params
+          p"====================="
+          @micropost = user.microposts.build(microposts_params)
+          p"====================="
+          p params
+          p"====================="                
+  
+          if @micropost.save 
+               render json: {status: 201,data: @micropost }
+          else
+              render json: {status: 422,errors: post.errors.full_messages}
+          end
+      end
+      def update
+          #p"====================="
+          #p params
+          #p"====================="
+          micropost=Micropost.find_by(id:params[:id])
+          #p"====================="
+          #p params
+          #p"====================="
+          if micropost.update(microposts_params)
+            p"====================="
+            p params
+            p"====================="
+            render json:{status: 201,data: micropost }
+          else
+            render json:{status: 400,error:"posts not update"}
+          end
+      end
+      def destroy
+          p"====================="
+          p params
+          p"====================="
+          micropost=Micropost.find_by(id:params[:id])
+          micropost.destroy
+          render json: { message: 'Micropost deleted successfully' }
+      end
   
     private
   
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+microposts = [] # 空の配列を作成
+
+10000.times do |n| # 10000回の繰り返しを行うループ
+  title = Faker::Lorem.sentence(word_count: 5) # ランダムな5単語で構成されるタイトルを生成
+  body = Faker::Lorem.sentence(word_count: 100) # ランダムな100単語で構成される本文を生成
+  user_id = User.pluck(:id).sample # Userモデルからランダムなuser_idを取得
+
+  # タイトルと本文とuser_idを使用してMicropostオブジェクトを作成し、データベースに保存する
+  micropost = Micropost.create(title: title, body: body, user_id: user_id)
+  # タイトルと本文をハッシュとして作成し、配列に追加する
+  microposts << micropost
+  #microposts << { title: title, body: body }
+  puts "#{n} create done"
+end
+
+# `insert_all`メソッドを使用して、`posts`配列の要素を一括でデータベースに挿入する
+
+#Micropost.insert_all(microposts) #rails 6以上使用可能
+#Micropost.create(microposts)
