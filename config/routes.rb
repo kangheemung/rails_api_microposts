7f2afe20245d5ad6717f597a9b17fa6680d5adc4
EC2 Default User <ec2-user@ip-10-0-0-35.ap-northeast-1.compute.internal>
2024-02-28 14:09:28 +0000

    relationships

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,22 @@
 Rails.application.routes.draw do
-  #post 'authenticate', to: 'authentication#authenticate'#endpointを揃うため移動
-
   namespace :api do
     namespace :v1 do
-      post 'auth'=>'auth#create'
+      post 'auth' => 'auth#create'
       delete 'auth' => 'auth#destroy'
+
       resources :users do
+        # Nested routes for microposts related to a user
         resources :microposts, except: [:index]
+
+        # Member routes for follow and unfollow actions
+        member do
+          post 'follow', to: 'relationships#follow'
+          delete 'unfollow', to: 'relationships#unfollow'
+        end
       end
 
       # Define the index route for microposts separately
-      get 'microposts' => 'microposts#index'
+      get 'microposts', to: 'microposts#index'
     end
   end
 end
