8facf8f741b1bc722a004738ffe99ea5feb0cb35
EC2 Default User <ec2-user@ip-10-0-0-35.ap-northeast-1.compute.internal>
2024-02-28 14:56:40 +0000

    follow

--- db/migrate/20231019071730_create_users.rb
+++ db/migrate/20231019071730_create_users.rb
@@ -7,12 +7,5 @@ class CreateUsers < ActiveRecord::Migration[5.2]
 
       t.timestamps
     end
-    # Adding index can improve lookup efficiency when querying relationships
-    #効率のため、
-    add_index :relationships, :follower_id
-    add_index :relationships, :followed_id
-    #ゆーさー１人のおいて１回選択のため、記入
-    # Optional: add an index that enforces uniqueness of the relationship to ensure a user can't follow another user more than once.
-    add_index :relationships, [:follower_id, :followed_id], unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_02_28_022548) do
+ActiveRecord::Schema.define(version: 2024_02_28_141645) do
 
   create_table "microposts", force: :cascade do |t|
     t.string "title"
